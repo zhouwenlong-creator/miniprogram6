006d68589674172a7f3de6cd0b3e4e0e9a13c35f
unknown <2567585172@qq.com>
2022-04-16 21:23:14 +0800

    4.16,平面图界面优化

--- pages/bespeak/bespeak.js
+++ pages/bespeak/bespeak.js
@@ -1,6 +1,46 @@
 // pages/bespeak/bespeak.js
 var information = getApp().globalData.chairInfo;
 // console.log(information);
+
+const date = new Date();//获取系统日期
+const years = [];
+const months = [];
+const days = [];
+const hours = [];
+const minutes = [];
+const bigMonth = [1, 3, 5, 7, 8, 10, 12];
+//将日期分开写入对应数组
+//年
+years.push(date.getFullYear());
+//月
+months.push(date.getMonth()+1);
+//日
+var day = date.getDate();
+console.log(day)
+for (let i = day; i <= 31; i++) {
+days.push(i);
+}
+// 获取小时
+var hour=date.getHours();
+console.log(hour);
+for (let i = hour; i <= 23; i++) {
+var k = i;
+if (0 <= i && i < 10) {
+k = "0" + i;
+}
+hours.push(k)
+}
+// 获取分钟
+var minute=date.getMinutes();
+console.log(minute);
+for (let i = minute; i <= 59; i++) {
+var k = i;
+if (0 <= i && i < 10) {
+k = "0" + i
+}
+minutes.push(k)
+}
+
 Page({
 
   /**
@@ -16,7 +56,101 @@ Page({
     chairInfo:[],
 
     //设置房间的编号（那个房间）
-    roomtype:"2"
+    roomtype:"0",
+
+    // 跟日期时间相关的变量
+    years: years,
+    year: date.getFullYear(),
+    months: months,
+    month: date.getMonth(),
+    days: days,
+    day: date.getDate(),
+    value: [9999, 1, 1],
+    hours: hours,
+    hour: date.getHours(),
+    minutes: minutes,
+    minute: date.getMinutes(),
+
+    // 设置预约时长数组
+    time:[
+      {
+        id:1,
+        name:"1小时"
+      },
+      {
+        id:2,
+        name:"2小时"
+      },
+      {
+        id:3,
+        name:"3小时"
+      },
+      {
+        id:4,
+        name:"4小时"
+      },
+      {
+        id:5,
+        name:"5小时"
+      },
+      {
+        id:6,
+        name:"6小时"
+      },
+      {
+        id:7,
+        name:"7小时"
+      },
+      {
+        id:8,
+        name:"8小时"
+      },
+      {
+        id:9,
+        name:"9小时"
+      },
+      {
+        id:10,
+        name:"10小时"
+      },
+      {
+        id:11,
+        name:"11小时"
+      },
+      {
+        id:12,
+        name:"12小时"
+      },
+    ],
+
+    room:[
+      {
+        roomtype:0,
+        roomname:"公共区",
+        isactive:true
+      },
+      {
+        roomtype:1,
+        roomname:"电脑屋",
+        isactive:false
+      },
+      {
+        roomtype:2,
+        roomname:"小黑屋",
+        isactive:false
+      },
+      {
+        roomtype:3,
+        roomname:"独立屋",
+        isactive:false
+
+      },
+      {
+        roomtype:4,
+        roomname:"小白屋",
+        isactive:false
+      }
+    ]
    },
 
   /**
@@ -80,11 +214,28 @@ Page({
 
   // 选座位界面切换房间
   onRoom:function(e){
-    var roomtype = e.target.dataset.roomtype;
+    console.log("切换平面图...");
+    //接受前台传来的那个room
+    var roomtypes = e.target.dataset.roomtype;
+    var roomlength=this.data.room.length;
+    var i=0;
+    for(i=0;i<roomlength;i++){
+      if(this.data.room[i].isactive==true)
+      //每次只有一个为选中的状态
+        this.setData({
+          //修改数组中的元素
+          [`room[${i}].isactive`]:false,
+        })
+      if(this.data.room[i].roomtype==roomtypes){
+        this.setData({
+          [`room[${i}].isactive`]:true,
+        })
+      }
+    };
     this.setData({
-      roomtype:roomtype
-    })
-    console.log(this.data.roomtype);
+      //用于刷新座位图
+      roomtype:roomtypes
+    });
   },
 
 
@@ -101,5 +252,85 @@ Page({
       [cha2]:"chair_4"
 
     })
+  },
+  // 选择日期时间
+  datesubmit:function(e){
+    // 点击之后背景变灰色
+    this.setData({
+      showModal: true
+      })
+  },
+  // 在showModel中点击确定
+  go: function () {
+    this.setData({
+    showModal: false
+    })
+    //后续操作
+  },
+  // 时长选取
+  timesubmit:function(e){
+    console.log("timesubmit...");
+    // 点击之后背景变灰色
+    this.setData({
+      showModal1: true
+      })
+  },
+  
+  //判断元素是否在一个数组
+  contains: function (arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) {
+      return true;
+      }
+    }
+    return false;
+  },
+
+  setDays: function (day) {
+    const temp = [];
+    for (let i = 1; i <= day; i++) {
+    temp.push(i);
+    }
+    this.setData({
+      days: temp
+    })
+  },
+  //选择滚动器改变触发事件
+  bindChange: function (e) {
+    const val = e.detail.value;
+    console.log(e);
+    //判断月的天数
+    const setYear = this.data.years[val[0]];
+    const setMonth = this.data.months[val[1]];
+    const setDay = this.data.days[val[2]];
+    const setHour = this.data.hours[val[3]];
+    const setMinute = this.data.minutes[val[4]];
+    console.log(setYear + '年' + setMonth + '月' + setDay + '日');
+    //闰年
+    if (setMonth === 2) {
+      if (setYear % 4 === 0 && setYear % 100 !== 0) {
+      // console.log('闰年')
+      this.setDays(29);
+      }else {
+      // console.log('非闰年')
+      this.setDays(28);
+      }
+    } else {
+    //大月
+    if (this.contains(bigMonth, setMonth)) {
+      this.setDays(31)
+    } else {
+      this.setDays(30)
+    }
   }
+    this.setData({
+    year: setYear,
+    month: setMonth,
+    day: setDay,
+    hour: setHour,
+    minute: setMinute
+    })
+  }
+
 })
