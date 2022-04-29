9d89dc649b17b43c14f64bff07b8531647bf4d93
zwl <zwlong6@163.c0m>
2022-04-29 13:43:43 +0800

    4.29

--- pages/manageuserdetail/manageuserdetail.js
+++ pages/manageuserdetail/manageuserdetail.js
@@ -1,4 +1,5 @@
 // pages/manageuserdetail/manageuserdetail.js
+var url=getApp().globalData.wxRequestBaseUrl;
 Page({
 
   /**
@@ -13,11 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;
-    var userInfoTarget=wx.getStorageSync('userInfoTarget');
-    that.setData({
-      userInfoTarget:userInfoTarget
-    })
+
   },
 
   /**
@@ -31,7 +28,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that=this;
+    var userInfoTarget=wx.getStorageSync('userInfoTarget');
+    that.setData({
+      userInfoTarget:userInfoTarget
+    })
   },
 
   /**
@@ -67,5 +68,92 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 功能
+  // 拉黑
+  onBlacklist(e){
+    var that=this;
+    wx.request({
+      url: url+'/user/updateUserStatusById.do',
+      method:'POST',
+      header: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
+      data:{
+        "id":e.currentTarget.dataset.id,
+        "status":1,
+      },
+      success(res){
+        console.log("拉黑成功");
+        wx.showToast({
+          title: '拉黑成功!',
+          icon:'success',
+          duration:1000,
+          success(){
+            setTimeout(function(){
+              that.setData({
+                [`userInfoTarget.status`]:1,
+              })
+              console.log(that.data.userInfoTarget.status);
+              that.onLoad();
+            },1000)
+          }
+        })
+      },
+      fail(res){
+        console.log("拉黑成功");
+        wx.showToast({
+          title: '拉黑失败,请稍后再试!',
+          icon:'error',
+          duration:1000,
+          success(){
+            setTimeout(function(){
+              that.onLoad();
+            },1000)
+          }
+        })
+      }
+    })
+    that.onShow();
+  },
+  onUnBlacklist(e){
+    var that=this;
+    wx.request({
+      url: url+'/user/updateUserStatusById.do',
+      method:'POST',
+      header: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
+      data:{
+        "id":e.currentTarget.dataset.id,
+        "status":0,
+      },
+      success(res){
+        console.log("解除拉黑成功");
+        wx.showToast({
+          title: '解除拉黑成功!',
+          icon:'success',
+          duration:1000,
+          success(){
+            setTimeout(function(){
+              that.setData({
+                [`userInfoTarget.status`]:0,
+              })
+              that.onLoad();
+            },1000)
+          }
+        })
+      },
+      fail(res){
+        console.log("解除拉黑失败");
+        wx.showToast({
+          title: '解除拉黑失败,请稍后再试!',
+          icon:'error',
+          duration:1000,
+          success(){
+            setTimeout(function(){
+              that.onLoad();
+            },1000)
+          }
+        })
+      }
+    })
   }
 })
